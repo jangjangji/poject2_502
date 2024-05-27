bce1942be7acabff50d46eb55f2624e327888c51
jangjangji <sjjang41@naver.com>
2024-05-27 10:20:52 +0900

    세팅완료

---
+++ src/main/java/org/choongang/Application.java
@@ -0,0 +1,8 @@
+package org.choongang;
+
+public class Application {
+    //jar파일 배포 했을때 이 main메서드가 젤 먼저 실행될것
+    public static void main(String[] args) {
+        System.out.println("프로그램 시작!");
+    }
+}
