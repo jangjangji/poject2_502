cdef9bec44214ec1a327536aaf87206d9116e28c
jangjangji <sjjang41@naver.com>
2024-05-27 14:59:54 +0900

    기본세팅완료

--- src/main/java/org/choongang/Application.java
+++ src/main/java/org/choongang/Application.java
@@ -1,8 +1,9 @@
 package org.choongang;
 
+import org.choongang.main.MainRouter;
+
 public class Application {
-    //jar파일 배포 했을때 이 main메서드가 젤 먼저 실행될것
     public static void main(String[] args) {
-        System.out.println("프로그램 시작!");
+        MainRouter.getInstance().start();
     }
 }
